967bab70376186c66302ceebeae801ba6076dd91
student <student@centos7.local>
2018-10-10 16:35:06 -0400

    Added bash script.

---
+++ Raven505fan/myRavenscript.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# This script asks a simple question
+# and responds based on the user input
+#
+# Let's ask out question
+echo "Who do you prefer in Fantasy Football, Odell Beckham  or Tyreek Hill ?"
+
+# Let's get their opinion
+# and assign it to the OPINION
+# variable
+choice=`echo $badchoice | tr '[:upper:]' '[:lower:]'`
+
+# Next, the script will respond based
+# on what they have entered
+#
+# NOTE: Bash is VERY picky.
+# Notice exactly where there are spaces
+#
+if [ "$choice" == "Odell Beckham" ]; then
+ echo "That's nice... But I like Tyreek Hill  better."
+ exit
+fi
+
+if [ "$choice" == "Tyreek Hill" ]; then
+ echo "Good deal! I like Tyreek Hill  better as well!"
+else
+exit
+
